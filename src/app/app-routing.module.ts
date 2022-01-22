4c0d1efd380ff36c5375cbc366571e65afa41d15
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-22 13:39:11 +0300

    add shopping cart files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCartComponent } from './Components/add-cart/add-cart.component';
+import { ProductsComponent } from './Components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"",redirectTo:'products',pathMatch:'full'
+  },
+  {
+    path:"products", component:ProductsComponent
+  },
+  {
+    path:"theCart", component:AddCartComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './Components/navigation/navigation.component';
+import { AddCartComponent } from './Components/add-cart/add-cart.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AddCartComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
